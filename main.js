b6ffbeedbf189847e2106baa544ce67074df70c6
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-12 13:08:56 -0600

    basic jQuery inclass exercise

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 console.log("I'm here!")
 $("h1").html("Hello friends");
+ var data;
+
+//append
+
+var foods = ["apple", "banana", "carrot", "danish", "egg"];
+
+for (var i= 0; i < foods.length; i++) {
+  $("ul").append(`<li>${foods[i]}</li>`);
+}
+
+$("ul").wrap(`<div class="foods"></div>`);
+
+$(":text").val("Thing I want it to be");
+
+$(":submit").click(function () {
+  console.log($(":text").val());
+});
+
+
+
+
+$.getJSON("https://randomuser.me/api/?nat=us", function (data) {
+  console.log(data);
+  $("h1").last().html(`hello ${data.results[0].name.first}`);
+});
